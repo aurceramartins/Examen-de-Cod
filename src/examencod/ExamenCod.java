9611e990c9bf907b64e11c02620b600b95f5765b
Alex Urcera <Alex Urcera@Alex>
2015-03-16 09:14:45 +0100

    LLamo a los metodos desde el main

--- src/examencod/ExamenCod.java
+++ src/examencod/ExamenCod.java
@@ -1,17 +1,15 @@
-
 package examencod;
 
 import twitter4j.TwitterException;
-
-
-
 import javax.swing.JOptionPane;
+
 public class ExamenCod {
 
-    
-    public static void main(String[] args)throws TwitterException  {
-       claseTwitter tweet = new claseTwitter();
-       
+    public static void main(String[] args) throws TwitterException {
+        claseTwitter tweet = new claseTwitter();
+        tweet.gettingTimeLine();
+        //tweet.consultarTweets(JOptionPane.showInputDialog(""));
+        //tweet.hacerUnTweet(JOptionPane.showInputDialog(""));
     }
-    
+
 }
