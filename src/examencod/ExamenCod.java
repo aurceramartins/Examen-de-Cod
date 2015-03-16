e67646f9984aef595d237e27a3fd430822203d4e
Alex Urcera <Alex Urcera@Alex>
2015-03-16 09:08:05 +0100

    Pego los consumer y los token en el programa

--- src/examencod/ExamenCod.java
+++ src/examencod/ExamenCod.java
@@ -1,13 +1,15 @@
 
 package examencod;
 
+import twitter4j.TwitterException;
 
 
 
+import javax.swing.JOptionPane;
 public class ExamenCod {
 
     
-    public static void main(String[] args) {
+    public static void main(String[] args)throws TwitterException  {
        claseTwitter tweet = new claseTwitter();
        
     }
--- src/examencod/claseTwitter.java
+++ src/examencod/claseTwitter.java
@@ -1,11 +1,21 @@
-
 package examencod;
-
-
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 public class claseTwitter {
-    
-    
-    
-    
-    
+
+    Twitter mitwitter;
+
+    public claseTwitter() {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("VoSmbUz4KtX6LWMADG2wjcai4")
+                .setOAuthConsumerSecret("l6UmkUXGVp7kARYmNGB0of5PsVuelFaokEm9rI4v5Fsjb9KZ7U")
+                .setOAuthAccessToken("3055290010-QWLhHK12ERY0t18MVAXQk4EfINP44CO9LtFP2Xq")
+                .setOAuthAccessTokenSecret("JsDAHOzJOk0YPlXQn56WOM6zpzei2nKvd4m1VerbVT2Gf");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        mitwitter = tf.getInstance();
+    }
+
 }
